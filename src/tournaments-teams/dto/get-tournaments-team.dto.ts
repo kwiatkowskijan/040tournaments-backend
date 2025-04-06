92606f9e981ffcdabfeb81cb4c97a823bf7a2765
kwiatkowskijan <70690975+kwiatkowskijan@users.noreply.github.com>
2025-04-06 19:13:42 +0200

    Add Dto mapping functions in Tournaments Teams

---
+++ src/tournaments-teams/dto/get-tournaments-team.dto.ts
@@ -0,0 +1,6 @@
+export class GetTournamentsTeamDto {
+    id: number;
+    tournamentId: number;
+    name: string;
+    city: string;
+}
--- src/tournaments-teams/tournaments-teams-db.service.ts
+++ src/tournaments-teams/tournaments-teams-db.service.ts
@@ -6,6 +6,8 @@ import { UpdateTournamentsTeamDto } from './dto/update-tournaments-team.dto';
 import { TournamentsTeam } from './entities/tournaments-team.entity';
 import { TournamentsTeamsService } from './tournaments-teams.service';
 import { TournamentsService } from 'src/tournaments/tournaments.service';
+import { GetTournamentsTeamDto } from './dto/get-tournaments-team.dto';
+import { Tournament } from 'src/tournaments/entities/tournament.entity';
 
 @Injectable()
 export class TournamentsTeamsDbService implements TournamentsTeamsService {
@@ -16,24 +18,29 @@ export class TournamentsTeamsDbService implements TournamentsTeamsService {
     async create(createTournamentsTeamDto: CreateTournamentsTeamDto) {
         const tournament = await this.tournamentsService.findOne(createTournamentsTeamDto.tournamentId);
 
-        const tournamentTeam: TournamentsTeam = {
-            id: 0,
-            tournament: tournament,
-            name: createTournamentsTeamDto.name,
-            city: createTournamentsTeamDto.city
-        }
+        const tournamentTeam: TournamentsTeam =
+            this.mapCreateTournamentsTeamDtoToEntity(createTournamentsTeamDto, tournament);
 
-        return await this.tournamentsTeamsRepository.save(tournamentTeam);
+        const tournamentsTeam: TournamentsTeam = await this.tournamentsTeamsRepository.save(tournamentTeam);
+
+        return this.mapTournamentsTeamToDto(tournamentsTeam);
     }
 
     async findAllByTournament(tournamentId: number) {
+        const getTournamentsTeamDto: GetTournamentsTeamDto[] = [];
 
-        return await this.tournamentsTeamsRepository.find({
+        const tournamentTeam: TournamentsTeam[] = await this.tournamentsTeamsRepository.find({
             relations: { tournament: true },
             where: {
                 tournament: { id: tournamentId }
             }
         });
+
+        tournamentTeam.forEach((tournamentsTeam) => {
+            getTournamentsTeamDto.push(this.mapTournamentsTeamToDto(tournamentsTeam))
+        })
+
+        return getTournamentsTeamDto;
     }
 
     async findOne(id: number) {
@@ -64,5 +71,23 @@ export class TournamentsTeamsDbService implements TournamentsTeamsService {
         }
 
         return this.tournamentsTeamsRepository.delete(id);
+    }
+
+    private mapTournamentsTeamToDto(tournamentsTeam: TournamentsTeam): GetTournamentsTeamDto {
+        return {
+            id: tournamentsTeam.id,
+            tournamentId: tournamentsTeam.tournament.id,
+            name: tournamentsTeam.name,
+            city: tournamentsTeam.city,
+        }
+    }
+
+    private mapCreateTournamentsTeamDtoToEntity(createTournamentsTeamDto: CreateTournamentsTeamDto, tournament: Tournament) {
+        return {
+            id: 0,
+            tournament: tournament,
+            name: createTournamentsTeamDto.name,
+            city: createTournamentsTeamDto.city
+        }
     }
 }
